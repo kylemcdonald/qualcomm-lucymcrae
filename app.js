7218ab8f5b935a18b20a94e10afd226032839a32
Kyle McDonald <kyle@kylemcdonald.net>
2015-05-23 03:57:43 -0400

    should process the raw data into a cleaned database

--- app.js
+++ app.js
@@ -2,17 +2,35 @@ var MongoClient = require('mongodb').MongoClient;
 var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
-var data;
+var rawCollection, dataCollection;
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-function add(obj, res) {
-  console.log(obj);
-  data.insert(obj, function(err, result) {
-    res.sendStatus(err ? 500 : 200);
+io.on('connection', function (socket) {
+  socket.emit('status', { server: __filename });
+  socket.on('status', function (data) {
+    console.log(data);
+  });
+});
+
+function add(raw, res) {
+  console.log(raw);
+  var data = {
+    serial: raw.device_serial_number,
+    time: raw.hub_receive_time,
+    spo2: raw.qcl_json_data.records[0].spO2.value,
+    hr: raw.qcl_json_data.records[0].pulseRate.value
+  }
+  dataCollection.insert(data, function(err1, result) {
+    rawCollection.insert(raw, function(err2, result) {
+      res.sendStatus((err1 || err2) ? 500 : 200);
+      io.sockets.emit('update', obj);     
+    })
   })
 }
 
@@ -24,8 +42,15 @@ app.post('/add', function (req, res) {
   add(req.body, res);
 });
 
-app.get('/get', function (req, res) {
-  data.find().toArray(function(err, items) {
+app.get('/get/raw', function (req, res) {
+  rawCollection.find().toArray(function(err, items) {
+    if(err) return res.sendStatus(500);
+    res.json(items);
+  });
+});
+
+app.get('/get/data', function (req, res) {
+  dataCollection.find().toArray(function(err, items) {
     if(err) return res.sendStatus(500);
     res.json(items);
   });
@@ -34,9 +59,10 @@ app.get('/get', function (req, res) {
 MongoClient.connect(process.env.MONGOLAB_URI, function(err, db) {
   if(err) { return console.dir(err); }
   console.log('Connected to database.');
-  data = db.collection('data');
+  rawCollection = db.collection('raw');
+  dataCollection = db.collection('data');
 
-  var server = app.listen(process.env.PORT, function () {
+  server.listen(process.env.PORT, function () {
     var host = server.address().address;
     var port = server.address().port;
     console.log('Listening at http://%s:%s', host, port);
